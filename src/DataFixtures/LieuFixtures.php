b3b1df6c2146772e672cfac7591fbc8110f030d4
grenard2024 <gabriel.renard2024@campus-eni.fr>
2024-11-05 16:48:12 +0100

    test fixture load

--- src/DataFixtures/LieuFixtures.php
+++ src/DataFixtures/LieuFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Lieu;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
 class LieuFixtures extends Fixture
@@ -20,22 +21,22 @@ class LieuFixtures extends Fixture
         $lieu = new Lieu();
         $lieu->setNom('Musée des Beaux-Arts');
         $lieu->setRue('20 Quai Emile Zola');
-        $lieu->setLatitude('47°,19,10,56 N');
-        $lieu->setLongitude('5°,02,20,16 E');
+        $lieu->setLatitude('47.19');
+        $lieu->setLongitude('5.02');
         $manager->persist($lieu);
 
         $lieu = new Lieu();
         $lieu->setNom('Opéra de Quimper');
         $lieu->setRue('PI de la Mairie');
-        $lieu->setLatitude('48°,6,40,11 N');
-        $lieu->setLongitude('1°,40,43,6 O');
+        $lieu->setLatitude('48.6,');
+        $lieu->setLongitude('5.02');
         $manager->persist($lieu);
 
         $lieu = new Lieu();
         $lieu->setNom('Opéra de Niort');
         $lieu->setRue('PI de la Mairie');
-        $lieu->setLatitude('48°,6,50,11 N');
-        $lieu->setLongitude('1°,10,43,6 O');
+        $lieu->setLatitude('48.6');
+        $lieu->setLongitude('1.10');
         $manager->persist($lieu);
 
         $manager->flush();
